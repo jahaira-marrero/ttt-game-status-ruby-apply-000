6fed33f669ab27978ef4f89a81bac9b171bb58a5
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-10-14 00:47:12 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,23 @@
 # Helper Method
 def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
+  if (board[index] == "") || (board[index] == " ") || (board[index] == nil)
+    taken = false
+  else
+    taken = true
+  taken  
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2] #toprow
+  [3,4,5] #middlerow
+  [6,7,8] #bottomrow
+  [0,3,6] #leftcolumn
+  [1,4,7] #middlecolumn
+  [2,5,8] #rightcolumn
+  [0,4,8] #leftdiagonal
+  [2,4,6] #rightdiagonal
+  ]
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "," "]
+
