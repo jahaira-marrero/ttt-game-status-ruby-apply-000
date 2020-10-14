b3287107b15346ef4699a1c807eb0074bda28a0e
Jahaira Marrero <jahaira.marrero@gmail.com>
2020-10-14 01:35:06 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,15 +9,15 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], #toprow
-  [3,4,5], #middlerow
-  [6,7,8], #bottomrow
-  [0,3,6], #leftcolumn
-  [1,4,7], #middlecolumn
-  [2,5,8], #rightcolumn
-  [0,4,8], #leftdiagonal
-  [2,4,6] #rightdiagonal
-  ]
+   [0, 1, 2],
+   [3, 4, 5],
+   [6, 7, 8],
+   [0, 4, 8],
+   [2, 4, 6],
+   [0, 3, 6],
+   [1, 4, 7],
+   [2, 5, 8]
+   ]
 
  def won?(board)
     WIN_COMBINATIONS.each do |i|
